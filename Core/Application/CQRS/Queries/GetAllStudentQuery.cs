9df6db6bfe9b5b49fcd793e79ec2dd73b650abee
DastanZhas <dastanzhas01@gmail.com>
2021-11-29 22:41:58 +0600

    Clean arhitecture+cqrs

---
+++ Core/Application/CQRS/Queries/GetAllStudentQuery.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.CQRS.Queries
+{
+    public class GetAllStudentQuery : IRequest<IEnumerable<Student>>
+    {
+        public class GetAllStudentQueryHandler : IRequestHandler<GetAllStudentQuery, IEnumerable<Student>>
+        {
+            private readonly IAppDbContext context;
+
+            public GetAllStudentQueryHandler(IAppDbContext context)
+            {
+                this.context = context;
+            }
+
+            public async Task<IEnumerable<Student>> Handle(GetAllStudentQuery query, CancellationToken cancellationToken)
+            {
+                var studentList = await context.Students.ToListAsync();
+
+                if(studentList == null)
+                {
+                    return null;
+                }
+                return studentList.AsReadOnly();
+            }
+        }
+    }
+}
---
+++ Infrastructure/Persistence/DependencyInjection.cs
@@ -0,0 +1,23 @@
+﻿using Application;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public static class DependencyInjection
+    {
+        public static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly("WebApi")));
+
+            //services.AddDbContext<AppDbContext>(options =>
+            //options.UseSqlServer("Data Source=localhost;Initial Catalog=SimpleWebDb;Integrated Security=True"));
+
+            services.AddScoped<IAppDbContext>(provider => provider.GetService<AppDbContext>());
+        }
+    }
+}
